6a871730076df741cbe8fc5a27a05ce583fb2430
Rodrigo Costa <rodrigogracacosta@hotmail.com>
2022-06-28 22:45:48 +0100

    Tipo

---
+++ ToDoList/Controllers/ToDoTypesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.DTO;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetToDoTypeDTO>>> Get()
+        {
+            return await _context.ToDoType.Select(x => new GetToDoTypeDTO { Id = x.Id, Descricao = x.Descricao }).ToListAsync();
+        }
+    }
+}
---
+++ ToDoList/DTO/GetToDoTypeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.DTO
+{
+    public class GetToDoTypeDTO
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
